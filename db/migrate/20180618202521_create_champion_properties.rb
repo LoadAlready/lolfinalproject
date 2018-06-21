26db5f36822f4e31ba1160a677181b3ede5db9ab
github username <github email address>
2018-06-21 15:54:42 -0400

    working site missing relationship between champ properties and properties

--- db/migrate/20180618202521_create_champion_properties.rb
+++ db/migrate/20180618202521_create_champion_properties.rb
@@ -2,7 +2,8 @@ class CreateChampionProperties < ActiveRecord::Migration[5.2]
   def change
     create_table :champion_properties do |t|
       t.integer :champion_id
-      t.integer :attribute_id
+      t.integer :property_id
+      t.integer :category_id
 
       t.timestamps
     end
